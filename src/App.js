6a8578edbd39740812e3e930e1e99ccb1bcde5e0
Erik Pastuszek <erikpastuszek@gmail.com>
2022-07-27 23:38:38 -0300

    deploy fix

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export function App() {
     <div className="App">
     <Nav/>
     <Routes>
-      <Route path="/"  element={<Home/>}/>
+      <Route path="/rickandmorty"  element={<Home/>}/>
       <Route path="/characters/:id"  element={<CardDetail/>}/>
     </Routes>
     </div>
