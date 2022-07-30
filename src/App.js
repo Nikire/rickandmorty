a7ab3a4adeacbd203aaed2d8eaeab5c23e70ac92
Erik Pastuszek <erikpastuszek@gmail.com>
2022-07-29 23:30:57 -0300

    fixed routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react'
 import './App.css';
-import {Route,Routes, useNavigate} from 'react-router-dom'
+import {Route,Routes} from 'react-router-dom'
 import Nav from './components/Nav/Nav'
 import Home from './components/Home/Home'
 import CardDetail from './components/CardDetail/CardDetail';
-
+import SoftNotFound from './components/SoftNotFound/SoftNotFound';
 export function App() {
-  const navigate=useNavigate();
   return (
     <div className="App">
     <Nav/>
     <Routes>
-      <Route path="/rickandmorty"  element={<Home/>}/>
-      <Route path="/rickandmorty/characters/:id"  element={<CardDetail/>}/>
+      <Route path="/home"  element={<Home/>}/>
+      <Route path="/characters/:id"  element={<CardDetail/>}/>
       <Route
       path="*"
-      element={
-        React.useEffect(()=>navigate('/rickandmorty'),[])
-      }
+      element={<SoftNotFound/>}
     />
     </Routes>
     </div>
---
+++ src/components/SoftNotFound/SoftNotFound.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export default function SoftNotFound() {
+    const navigate= useNavigate();
+    const redirect= ()=>(setTimeout(()=>navigate('/home'),3000))
+
+    useEffect(()=>{redirect()},[])
+
+    return (
+        <div>Página no encontrada,redirigiendo en 3 segundos al home...</div>
+    )
+}
