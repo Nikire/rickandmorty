29748bf80ed7580243f5a8f65a657ce71d6c81b9
Erik Pastuszek <erikpastuszek@gmail.com>
2022-07-30 00:06:26 -0300

    routes fix 5

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export function App() {
     <div className="App">
     <Nav/>
     <Routes>
-      <Route path="/rickandmorty/"  element={<Home/>}/>
+      <Route path="/rickandmorty"  element={<Home/>}/>
       <Route path="/rickandmorty/characters/:id"  element={<CardDetail/>}/>
       <Route
       path="*"
